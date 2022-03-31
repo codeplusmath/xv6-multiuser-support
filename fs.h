82b14734e08eadb464c1eb271e22c2d382d0100b
RajkumarSawant <sawantrv19.comp@coep.ac.in>
2022-03-31 22:18:34 +0530

    Adding uid & mode to On disk file inode

--- fs.h
+++ fs.h
@@ -33,6 +33,9 @@ struct dinode {
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
   uint addrs[NDIRECT+1];   // Data block addresses
+
+	uint mode;	// mode of file
+	unit uid;	// owner id
 };
 
 // Inodes per block.
