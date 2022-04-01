209d24a8ace0c18566b6f59e1a8785bb0aaa693e
gaidhanisanjyot <gaidhanisanjyot@gmail.com>
2022-04-01 11:51:34 +0530

    definig prototypes - getuid,setuid,geteuid

--- syscall.c
+++ syscall.c
@@ -103,6 +103,9 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_getuid(void); //defining the protypes for sys calls
+extern int sys_geteuid(void); //
+extern int sys_setuid(void);  //
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -126,6 +129,9 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getuid]    sys_getuid,
+[SYS_geteuid]   sys_geteuid,
+[SYS_setuid]    sys_setuid
 };
 
 void
